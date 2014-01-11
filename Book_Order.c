83ce7707b9a3e66cfad4c2e60d76454666ebb452
ddurbin <ddurbin@users.noreply.github.com>
2014-01-11 13:10:46 -0500

    Update Book_Order.c

--- Book_Order.c
+++ Book_Order.c
@@ -1,93 +1,6 @@
-#include "Book_Order.h"
+#include "book_order.h"
 
-customer **processDatabase(FILE *database){
-
-	int i;
-	char c;
-	char *string = (char*)malloc(sizeof(char));
-	string = "";
-    c = getc(database);
-    while(c != EOF){  
-            
-        string = Concat(string, c);
-        c = getc(database);
-    }
-	
-	customer **customerArr = (customer**)malloc(100*sizeof(customer*));
-
-	char *token, *hold, *seps;
-	int index;
-	hold = NULL;
-	seps = "|\n";
-	token = strtok_r(string, seps, &hold);
-	while(token != NULL){
-
-		customer *new = (customer*)malloc(sizeof(customer));
-		new->name = token;
-		token = strtok_r(NULL, seps, &hold);
-		new->customerID = token;
-		token = strtok_r(NULL, seps, &hold);
-		new->balance = token;
-		token = strtok_r(NULL, seps, &hold);
-		new->address = token;
-		token = strtok_r(NULL, seps, &hold);
-		new->state = token;
-		token = strtok_r(NULL, seps, &hold);
-		new->zip = token;
-		new->purchasedList = NULL;
-		new->rejectedList = NULL;;
-		customerArr[getIndex(new)] = new;
-		token = strtok_r(NULL, seps, &hold);
-	}
-	return customerArr;
-}
-
-orderQueue *buildQueue(FILE *order){
-    char c;
-    char *string = (char*)malloc(sizeof(char));
-    string = "";
-    c = getc(order);
-    while(c != EOF){
-        string = Concat(string, c);
-        c = getc(order);
-    }
-    orderQueue *queue = (orderQueue*)malloc(sizeof(orderQueue));
-    queue->front = NULL;
-    queue->rear = NULL;
-    
-    char *token, *hold, *seps;
-    hold = NULL;
-    seps = "|\n";
-    token = strtok_r(string, seps, &hold);
-    while(token != NULL){
-        
-        orderNode *new = (orderNode*)malloc(sizeof(orderNode));
-        new->title = token;
-        token = strtok_r(NULL, seps, &hold);
-        new->price = token;
-        token = strtok_r(NULL, seps, &hold);
-        new->customerID = token;
-        token = strtok_r(NULL, seps, &hold);
-        new->category = token;
-        token = strtok_r(NULL, seps, &hold);
-        new->next = NULL;
-        if(queue->front == NULL){
-            queue->front = new;
-            queue->rear = new;
-        }
-        else {
-            queue->rear->next = new;
-            queue->rear = new;
-        }
-    }
-    return queue;
-}
-
-//determine customer array index given new cutomer instance
-int getIndex(customer *x){
-
-	return atoi(x->customerID) % 100;
-}
+static pthread_t *tids; //static array of thread ID's for pthread_wait function calls
 
 char *Concat(char *string, char letter){    //concatenate character to end of a string
     char *str = string;
@@ -99,98 +12,63 @@ char *Concat(char *string, char letter){    //concatenate character to end of a 
     return result;
 }
 
-int numCategories(FILE *fp){
-
-	 int ch;
-	 int numLines= 0;
-	do{
-
-		ch = fgetc(fp);
-		if(ch == '\n'){
-			numLines++;
-		}
-	}while(ch != EOF);
-
-	if(numLines != 0){
-		numLines++;
-	}
-	return numLines;
-}
-
-unsigned int createThread(char *string){
-	pthread_t newThread;
-	numThreads++;
-	return pthread_create(&newThread, NULL, processCategory, (void*)string);
-}
-
-void *processCategory(void *arg){
-	char* category = (char*)arg;
-	printf("Category is: %s\n", category);
-	numThreads--;
-	return NULL;
-}
-
-
 
 int main(int argc, char** argv){
+    totalRevenue = 0;
+    database = fopen(argv[1], "r"); order = fopen(argv[2], "r");
+    if(database == NULL || order == NULL){ //file error checking
 
-	numThreads = 0;
-	customerArr = (customer**)malloc(100*sizeof(customer*));
-	database = fopen(argv[1], "r");
-	order = fopen(argv[2], "r");
-	categories = fopen(argv[3], "r");
-	if(database == NULL || order == NULL || categories == NULL){
+        printf("***Error opening files***\n");
+        return 1;
+    }
 
-		printf("***Error opening files***\n");
-		return 1;
-	}
-	customer **arr = processDatabase(database);
-	orderQueue *queue = buildQueue(order);
+    if(pthread_mutex_init(&lock, NULL) != 0){ //initialize mutex lock
+                
+        printf("***Mutex init failed***\n");
+    }
+
+    customerArr = processDatabase(database); //customer array initialization
+    if(customerArr == NULL){ //check for empty database
+        printf("***Error creating database***\n");
+        return 1;
+    }
+    queue = buildQueue(order); //build order queue with order file
+    
     int i;
-    for(i=0;i<100;i++){
-        if(arr[i] != NULL){
-            printf("name: %s\nID: %s\nBalance: %.2f\nAddress: %s\nState: %s\nZip: %s\n", arr[i]->name, arr[i]->customerID, atof(arr[i]->balance), arr[i]->address, arr[i]->state, arr[i]->zip);
+    char *categories = (char*) argv[3];
+    tids = (pthread_t*)malloc(numCategories(categories)*sizeof(pthread_t)); //initialize pthread array for each category
+    tidIndex = 0; //thread ID array index
+    int index = 0; //Categories string index
+    char *string = malloc(sizeof(char));
+    string = "";
+    char c;
+        
+    do{ //create thread for each category
+        c = categories[index];
+        if(c  == ' ' || c == '\0'){ // new category, create a thread
+            if(pthread_create(&tids[tidIndex], NULL, processCategory, (void*)string) != 0){  //create thread passing category argument
+                                
+                printf("***Error creating new thread***\n");
+                return 1;
+            }
+        tidIndex++;
+        string = "";
+        }else{ //not a new category, add character to string
+            string = Concat(string, c);
         }
+    index++;       
+    }while(c != '\0');
+
+    for(i=0; i<numCategories(categories); i++){ // Main thread joins all the category threads and waits until finished
+       pthread_join(tids[i],NULL);
     }
-    orderNode *ptr = queue->front;
-    while(ptr != NULL){
-        printf("title: %s\nprice: %s\nID: %s\ncategory: %s\n\n", ptr->title, ptr->price, ptr->customerID, ptr->category);
-        ptr = ptr->next;
-    }
-	for(i=0;i<100;i++){
-		if(arr[i] != NULL){
-
-			printf("name: %s\nID: %s\nBalance: %.2f\nAddress: %s\nState: %s\nZip: %s\n", arr[i]->name, arr[i]->customerID, atof(arr[i]->balance), arr[i]->address, arr[i]->state, arr[i]->zip);
-		}
-	}
-	
-	int thread;
-	char *string = malloc(sizeof(char));
-	string = "";
-	char c;
-	int ch;
-	
-	do{
-		c = getc(categories);
-		ch = (int)c;
-		if(ch  == '\n' || ch == EOF){
-			thread = createThread(string);
-			if(thread != 0){
-				
-				printf("***Error creating new thread***\n");
-				return 1;
-			}
-			string = "";
-		}else{
-			string = Concat(string, c);
-		}
-		
-	}while(ch != EOF);
-
-	while(numThreads != 0){
-
-		sleep(1);
-	}
-
-	return 0;
+    
+    output(customerArr); //print out final report
+    //free allocated memroy
+    freeCustomers(customerArr);
+    freeQueue(queue);
+    free(tids);
+    fclose(database);
+    fclose(order);
+    return 0;
 }
